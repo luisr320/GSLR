0b5ac7cadc0783f98dc139a9e48c62e4f6fd8af8
luisr320 <luisr320@gmail.com>
2016-07-14 00:01:41 +0100

    Edited the ISR, removing the UDR0

--- GSLR.ino
+++ GSLR.ino
@@ -964,13 +964,7 @@ char* fill(char* str, int length, char charcode, bool initialize)
 }
 
 // Interrupt is called once a millisecond, looks for any new GPS data, and stores it
-SIGNAL(TIMER0_COMPA_vect) {
-		char c = GPS.read();
-		Serial.println(c);
-		// if you want to debug, this is a good time to do it!
-#ifdef UDR0
-				if (c) UDR0 = c;
-		// writing direct to UDR0 is much much faster than Serial.print
-		// but only one character can be written at a time.
-#endif
+SIGNAL(TIMER0_COMPA_vect)
+{
+		GPS.read();
 }
